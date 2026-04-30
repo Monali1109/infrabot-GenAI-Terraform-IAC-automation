99439774d086b48644a8e0f6e8ac0262395a8c98
Monali Patil <98048460+Monali1109@users.noreply.github.com>
2026-04-30 14:40:06 +0530

    terraform: add azure/test/main.tf

---
+++ azure/test/main.tf
@@ -0,0 +1,65 @@
+# ── Compute: gmol8tadv (test) ─────────────────────────────────
+variable "gmol8tadv_vm_size" {
+  description = "Azure VM size"
+  type        = string
+  default     = "Standard_D2s_v3"
+}
+variable "gmol8tadv_count" {
+  description = "Number of VMs"
+  type        = number
+  default     = 1
+}
+variable "gmol8tadv_os_disk_size" {
+  description = "OS disk size GB"
+  type        = number
+  default     = 128
+}
+
+data "azurerm_subnet" "gmol8tadv_subnet" {
+  name                 = "${var.subnet_name}"
+  virtual_network_name = "${var.vnet_name}"
+  resource_group_name  = var.resource_group_name
+}
+
+resource "azurerm_network_interface" "gmol8tadv_nic" {
+  count               = var.gmol8tadv_count
+  name                = "gmol8tadv-gmol8tadv-nic-${count.index}"
+  resource_group_name = var.resource_group_name
+  location            = var.azure_location
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.gmol8tadv_subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "gmol8tadv" {
+  count               = var.gmol8tadv_count
+  name                = "gmol8tadv-${count.index}"
+  resource_group_name = var.resource_group_name
+  location            = var.azure_location
+  size                = var.gmol8tadv_vm_size
+  admin_username      = "azureadmin"
+  network_interface_ids = [azurerm_network_interface.gmol8tadv_nic[count.index].id]
+
+  admin_ssh_key {
+    username   = "azureadmin"
+    public_key = var.ssh_public_key
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+    disk_size_gb         = var.gmol8tadv_os_disk_size
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+  identity { type = "SystemAssigned" }
+  tags = { environment = "test", generation = "gmol8tadv", name = "gmol8tadv" }
+}
+
+output "gmol8tadv_ids"         { value = azurerm_linux_virtual_machine.gmol8tadv[*].id }
+output "gmol8tadv_private_ips" { value = azurerm_network_interface.gmol8tadv_nic[*].private_ip_address }
