b2a9c563db731569e2f11368728c9aa9d15ce58d
Monali Patil <98048460+Monali1109@users.noreply.github.com>
2026-04-30 15:07:49 +0530

    terraform: add gcp/dev/main.tf

---
+++ gcp/dev/main.tf
@@ -0,0 +1,16 @@
+# ── IAM: gmolagv59 (dev) ── Who: auto-SA  |  Access: project-wide
+resource "google_service_account" "gmolagv59_sa" {
+  account_id   = "${var.project_name}-gmolagv59-sa"
+  display_name = "gmolagv59 SA (dev)"
+  project      = var.gcp_project_id
+}
+
+# Project-level IAM
+resource "google_project_iam_member" "gmolagv59_binding" {
+  project = var.gcp_project_id
+  role    = "roles/viewer"
+  member  = "serviceAccount:${google_service_account.gmolagv59_sa.email}"
+}
+
+output "gmolagv59_sa_email" { value = google_service_account.gmolagv59_sa.email }
+output "gmolagv59_member" { value = "serviceAccount:${google_service_account.gmolagv59_sa.email}" }
